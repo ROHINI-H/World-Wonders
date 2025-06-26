5171b577f613450f7c931bf9c183d0bb3bdef9ff
Rohini <h.rohini19@gmail.com>
2025-06-26 20:11:17 +0530

    Basic functionality Done

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function displayImage() {
+    const photos = ["images/Chichén Itzá.jpg",
+        "images/Christ the Redeemer.jpg",
+        "images/Colosseum.jpg",
+        "images/Great wall of China.jpg",
+        "images/Machu Picchu.jpg",
+        "images/Petra.jpg",
+        "images/Taj Mahal.jpg"];
+    
+        let i= 0;
+        setInterval(() => {
+            document.getElementsByTagName("img")[0].setAttribute("src", photos[i]);
+            document.getElementById("place").innerText = photos[i].slice(7,-4);
+            i++;
+            if(i == photos.length) {
+                i=0;
+            }
+        },2000);
+}
+
